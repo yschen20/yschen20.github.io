5663bbeee2b872eb4b460c201e5c1096726342c9
yschen20 <ysc19826085315@qq.com>
2025-06-18 21:12:18 +0800

    Site updated: 2025-06-18 21:12:14

---
+++ js/backgroundize.js
@@ -0,0 +1,19 @@
+document
+    .querySelector('#web_bg')
+    .setAttribute('style', `background-image: ${document.querySelector('.banner').style.background.split(' ')[0]};position: fixed;width: 100%;height: 100%;z-index: -1;background-size: cover;`);
+
+document
+    .querySelector("#banner")
+    .setAttribute('style', 'background-image: url()')
+
+document
+    .querySelector("#banner .mask")
+    .setAttribute('style', 'background-color:rgba(0,0,0,0)')
+
+/**
+ —— 转载请注明出处！
+ —— 原文作者：4rozeN
+ —— 原文标题：Hexo fluid 全屏背景图随日夜模式切换以及正文底页毛玻璃效果
+ —— 原文链接：https://4rozen.github.io/archives/Hexo/60191.html#%E5%AE%9E%E7%8E%B0%E8%83%8C%E6%99%AF%E5%9B%BE%E5%85%A8%E5%B1%8F%E6%95%88%E6%9E%9C
+ —— 原文字数：3.1k 字
+ */
--- js/events.js
+++ js/events.js
@@ -83,6 +83,10 @@ Fluid.events = {
     if (board.length === 0) {
       return;
     }
+    var arrowUpIcon = jQuery('#scroll-top-button i');
+    if(arrowUpIcon.length === 0){
+      return;
+    }
     var posDisplay = false;
     var scrollDisplay = false;
     // Position
@@ -92,8 +96,13 @@ Fluid.events = {
       var right = bodyWidth - boardRight;
       posDisplay = right >= 50;
       topArrow.css({
-        'bottom': posDisplay && scrollDisplay ? '20px' : '-60px',
-        'right' : right - 64 + 'px'
+        'bottom': scrollDisplay ? '20px' : '-60px',
+        'right' : posDisplay ? right - 64 : 8 + 'px',
+        'min-width' : posDisplay ? 40 : 28 + 'px',
+        'min-height' : posDisplay ? 40 : 28 + 'px'
+      });
+      arrowUpIcon.css({
+        'font-size' : posDisplay ? 32 : 20 + 'px'
       });
     };
     setTopArrowPos();
@@ -104,7 +113,7 @@ Fluid.events = {
       var scrollHeight = document.body.scrollTop + document.documentElement.scrollTop;
       scrollDisplay = scrollHeight >= headerHeight;
       topArrow.css({
-        'bottom': posDisplay && scrollDisplay ? '20px' : '-60px'
+        'bottom': scrollDisplay ? '20px' : '-60px'
       });
     });
     // Click
@@ -115,6 +124,7 @@ Fluid.events = {
       });
     });
   },
+
 
   registerImageLoadedEvent: function() {
     if (!('NProgress' in window)) { return; }
